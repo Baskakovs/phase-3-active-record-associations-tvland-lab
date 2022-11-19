e012bb520f9d8ba01d70ce1619ebc2c8802654e8
Baskakovs <baskakovs@me.com>
2022-11-19 16:52:42 +0000

    commit tree 3e81e745ecc060e646fb730fcfcb8a3706ffe267

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    "#{Character.first.name} - #{Character.first.show.name}"
+    # characters = self.characters.all
+    # characters.each do |character| 
+    #     character.name
+    # end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors = self.actors.all
+    full_names = []
+    actors.each do |actor|
+        full_names << "#{actor.first_name} #{actor.last_name}"
+    end
+    full_names
+  end
 end
